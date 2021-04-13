f4d8302b622acc75a7cc36b9aa8a876d1bf4c815
Ronald M <ronaldmaymone@id.uff.br>
2021-04-13 16:16:39 -0300

    [0.2.0] video playlist working and no overlays or appbar on fullscreen

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,53 +1,42 @@
-import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
 import 'package:vision_app/app/controllers/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Página Principal'), centerTitle: true, actions: [
-        PopupMenuButton<String>(
-          onSelected: Get.find<HomeController>().handlePopMenuClick,
-          itemBuilder: (BuildContext context) {
-            return {'Sobre', 'Configurações', Get.find<HomeController>().isLogged() ? 'Sair' : 'Entrar'}.map((String choice) {
-              return PopupMenuItem<String>(
-                value: choice,
-                child: Text(choice),
-              );
-            }).toList();
-          },
-        ),
-      ],),
+      // appBar: AppBar(title: Text('Página Principal'), centerTitle: true, actions: [
+      //   PopupMenuButton<String>(
+      //     onSelected: Get.find<HomeController>().handlePopMenuClick,
+      //     itemBuilder: (BuildContext context) {
+      //       return {'Sobre', 'Configurações', Get.find<HomeController>().isLogged() ? 'Sair' : 'Entrar'}.map((String choice) {
+      //         return PopupMenuItem<String>(
+      //           value: choice,
+      //           child: Text(choice),
+      //         );
+      //       }).toList();
+      //     },
+      //   ),
+      // ],),
       body: Container(
         child: GetBuilder<HomeController>(
             builder: (_) {
               print("STARTED GET BUILDER");
-              return _.chewieController != null ?
-                // AspectRatio(
-                //   aspectRatio: 16 / 9,
-                //   child: BetterPlayer(
-                //     controller: _.betterPlayerController,
-                //   ),
-                // ):
-                  AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: Chewie(
-                      controller: _.chewieController,
-                    ),
-                  ):
+              return _.videoPlayerController != null && _.videoPlayerController.value.isInitialized ?
+                GetX<HomeController>(
+                    builder: (_){ print("RELOADED VIDEO WIDGET");return !_.loadingNextVideo ? AspectRatio(
+                      aspectRatio: _.videoPlayerController.value.aspectRatio,
+                      child: VideoPlayer(
+                        _.videoPlayerController,
+                      ),
+                    ):
+                      Center(child: CircularProgressIndicator());
+                  }
+                ):
               GetX<HomeController>(builder:(_){ return Center(child: loadingWidget(_));});
-              // return _.list != null ? ListView.builder(
-              //   itemCount: _.list.files.length,
-              //   itemBuilder: (BuildContext context, int index){
-              //     return ListTile(
-              //       leading: Icon(Icons.file_download_done),
-              //       title: Text(_.list.files[index].name),
-              //     );
-              //   }
-              // ) : Center(child: CircularProgressIndicator(),);
             },
         )
       ),
